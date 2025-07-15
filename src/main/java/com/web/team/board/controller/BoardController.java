6dd4422d8565cbb1ec38d3a88af661587ce9a46f
seungjae100 <seungjaebaekmail@gmail.com>
2025-07-15 17:22:43 +0900

    swagger/board-#20: 공지사항 Swagger 문서화 진행

--- src/main/java/com/web/team/board/controller/BoardController.java
+++ src/main/java/com/web/team/board/controller/BoardController.java
@@ -9,6 +9,10 @@ import com.web.team.board.service.BoardService;
 import com.web.team.exception.CustomException;
 import com.web.team.exception.ErrorCode;
 import com.web.team.jwt.CustomAdminDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,11 +27,19 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/board")
+@Tag(name = "회사 공지사항 게시판 API", description = "회사 공지사항 관련 API 입니다.")
 @RequiredArgsConstructor
 public class BoardController {
 
     private final BoardService boardService;
 
+    @Operation(summary = "관리자 공지사항 등록", description = "관리자가 회사 공지사항을 등록합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "공지사항 등록 성공"),
+            @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
+            @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+            @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+    })
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     @RequestMapping("/create")
     public ResponseEntity<?> createBoard(
@@ -44,6 +56,14 @@ public class BoardController {
         );
     }
 
+    @Operation(summary = "공지사항 수정", description = "관리자가 공지사항을 수정합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "공지사항 수정을 완료하였습니다."),
+            @ApiResponse(responseCode = "400", ref = "#/components/responses/BadRequest"),
+            @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+            @ApiResponse(responseCode = "404", ref = "#/components/responses/NotFound"),
+            @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError"),
+    })
     // 게시글 수정
     @PatchMapping("/{uuid}")
     public ResponseEntity<?> updateBoard(
@@ -56,6 +76,12 @@ public class BoardController {
         return ResponseEntity.ok("게시글 수정 완료");
     }
 
+    @Operation(summary = "모든 공지사항 조회", description = "관리자가 등록한 공지사항을 전체 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "전체 공지사항 조회"),
+            @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+            @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+    })
     // 모든 게시글 목록 조회
     @GetMapping
     public ResponseEntity<List<BoardListResponse>> getAllBoards(@AuthenticationPrincipal Object userDetails) {
@@ -68,6 +94,13 @@ public class BoardController {
         return ResponseEntity.ok(result);
     }
 
+    @Operation(summary = "공지사항 세부내용 조회", description = "공지사항의 단 건에 대한 세부 내용을 확인할 수 있다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "공지사항 세부내용 조회 성공"),
+            @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+            @ApiResponse(responseCode = "404", ref = "#/components/responses/NotFound"),
+            @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+    })
     // 게시글 세부내용 조회
     @GetMapping("/{uuid}")
     public ResponseEntity<BoardDetailResponse> getBoard(
@@ -83,6 +116,13 @@ public class BoardController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "공지사항 게시글 삭제", description = "공지사항 게시글을 삭제합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "공지사항 삭제 성공"),
+            @ApiResponse(responseCode = "403", ref = "#/components/responses/Forbidden"),
+            @ApiResponse(responseCode = "404", ref = "#/components/responses/NotFound"),
+            @ApiResponse(responseCode = "500", ref = "#/components/responses/InternalServerError")
+    })
     // 게시글 삭제
     @DeleteMapping("{/uuid}")
     public ResponseEntity<?> deleteBoard(
--- src/main/java/com/web/team/board/dto/BoardListResponse.java
+++ src/main/java/com/web/team/board/dto/BoardListResponse.java
@@ -2,21 +2,31 @@ package com.web.team.board.dto;
 
 import com.web.team.board.domain.Board;
 import com.web.team.board.domain.BoardStatus;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class BoardListResponse {
+@Schema(description = "공지사항 목록 응답 DTO")
+public record BoardListResponse (
 
-    private String uuid;
-    private String title;
-    private String content;
-    private String writerName;
-    private LocalDateTime createdAt;
-    private BoardStatus status;
+        @Schema(description = "공지사항 UUID", example = "12ce3t2332...")
+        String uuid,
+
+        @Schema(description = "공지사항 제목", example = "대체공휴일 휴무 일정안내")
+        String title,
+
+        @Schema(description = "공지사항 내용", example = "1월 1일 공휴일이 주말인 관계로...")
+        String content,
+
+        @Schema(description = "공지사항 작성자", example = "관리자")
+        String writerName,
+
+        @Schema(description = "공지사항 작성일", example = "2025-07-25")
+        LocalDateTime createdAt,
+
+        @Schema(description = "공지사항 상태", example = "ACTIVE")
+        BoardStatus status
+)  {
 
     public static BoardListResponse from(Board board) {
         return new BoardListResponse(
